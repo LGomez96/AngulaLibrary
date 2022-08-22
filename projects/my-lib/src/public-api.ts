766e9dc4e439d6d9db43bcc9abfc48ef68d6ba40
LGomez96 <lindagomez96@gmail.com>
2022-08-22 16:54:09 -0500

    eliminamos lib y creacion de header

--- projects/my-lib/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of my-lib
- */
-
-export * from './header/header-service.service';
-export * from './header/header.component';
-export * from './header/header.module';
-// agregar los de los 3 componentes creados
